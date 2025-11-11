ab57a5e0f602f07ea1b30f67585007b34da66871
sincerelyyyash <yta0088@gmail.com>
2025-11-11 16:37:03 +0530

    feat(backend): bootstrap Actix-web server with /health and Postgres pool; add required dependencies

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+use dotenvy::dotenv;
+
+async fn health()-> impl Responder {
+    HttpResponse::Ok()
+    .content_type("application/json")
+    .body(r#"{"status": "Ok"}"#)
+}
+
+async fn run()-> std::io::Result<()> {
+
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+    .expect("DATABASE_URL must be set in .env");
+
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&database_url)
+    .await
+    .expect("Failed to create Postgres pool");
+
+    println!("Connected to Postgres Database");
+
+
+    HttpServer::new(move|| App::new()
+    .app_data(web::Data::new(pool.clone()))
+    .route("/health", web::get().to(health)))
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+fn main()-> std::io::Result<()> {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed to build Tokio runtime");
+    runtime.block_on(run())
 }
